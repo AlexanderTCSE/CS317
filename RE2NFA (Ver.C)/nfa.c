b42c1099960bb00375df1f9c828b17aae2a55755
AlexanderTCSE <tattersallalexd@gmail.com>
2024-10-08 14:10:01 -0700

    Stack functionality added, may need ironing out.

---
+++ RE2NFA (Ver.C)/nfa.c
@@ -0,0 +1,7 @@
+//Alexander Tattersall
+//CS317 - Professor Kabir
+//Stack code for RE processor
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"nfa.h"
--- RE2NFA (Ver.C)/stack.c
+++ RE2NFA (Ver.C)/stack.c
@@ -4,3 +4,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"stack.h"
+
+//Initialize an empty stack
+void init(struct Stack *stack){
+    stack -> top = -1;
+}
+
+//Check if stack is empty, return boolean T/F
+bool isEmpty(struct Stack *stack){
+    return stack->top == -1;
+}
+
+//Check if stack is full, return boolean T/F
+bool isFull(struct Stack *stack){
+    return stack->top == 1024-1;
+}
+
+//Push an NFA onto the stack
+void push(struct Stack *stack, struct NFA *nfa){
+    if(isFull(stack)){
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack -> arr[++stack->top] = nfa;
+}
+
+//Pop the top element of the stack
+int pop(struct Stack *stack){
+    if(isEmpty(stack)){
+        printf("Stack Underflow\n");
+        return;
+    }
+    int popped = stack->arr[stack->top];
+    stack->top--;
+    return popped;
+}
+
+//Peek at the top element of the stack
+int peek(struct Stack *stack){
+    if(isEmpty(stack)){
+        printf("Stack Empty\n");
+        return -1;
+    }
+    return stack->arr[stack->top];
+}
